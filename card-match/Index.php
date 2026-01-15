b9f008f1f51ef5cff69dff88ef4734e94bff844c
clara-cdp <clarianne.cdp@gmail.com>
2026-01-15 11:54:01 +0100

    what I did in 30 minutes

---
+++ card-match/Index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel=stylesheet href='styles.css'>
+    <title>Matching Cards</title>
+</head>
+
+<body>
+    <header>
+        <h1>Matching Up!</h1>
+    </header>
+    <div class='score'>your score:
+        <input id='scoreBox' type=text-field name='score' , placeholder="00.00"></imput>
+    </div>
+    <div class=' gameBoard'>
+
+        <!-- create cards in javaSccript-->
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+        <div class=cards></div>
+    </div>
+
+</body>
+
+</html>
