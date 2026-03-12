d92f99c5a3c7ca04fe56bcadb7a970a84da671aa
clara-cdp <clarianne.cdp@gmail.com>
2026-03-12 10:59:50 +0100

    what I did in 30 minutes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let flippedCards = [];
+let matchedCount = 0;
+
 
 const gameBoard = document.querySelector('.gameBoard');
 let cardCount = 16;     // => we could add more cards for a more challenging game.
@@ -9,11 +12,11 @@ if (!gameBoard) {
 let backOfCards = [
     "assets/card1.jpg",
     "assets/card2.jpg",
-     "assets/card3.jpg",
+    "assets/card3.jpg",
     "assets/card4.jpg",
-     "assets/card5.jpg",
+    "assets/card5.jpg",
     "assets/card6.jpg",
-     "assets/card7.jpg",
+    "assets/card7.jpg",
     "assets/card8.jpg",
 ]
 
@@ -23,29 +26,42 @@ allGameCards.sort(() => Math.random() - 0.5);
 
 for (let i = 0; i < cardCount; i++) {
 
-    const card= document.createElement('div');
+    const card = document.createElement('div');
     const currentImage = allGameCards[i];
-    
+
     card.classList.add('cards');
 
-card.innerHTML = `
+    card.innerHTML = `
     <div class="cardPosition">
         <div class="cardFront"></div>
         <div class="cardBack">
         <img style="object-fit:cover; "src="${currentImage}" alt="card-icon" ></div>
-    </div>
-`;
-    card.addEventListener('click', function () {
-          
-        if (!timerRunning) {
-        startTimer();
-        timerRunning = true; 
-    }
-        this.classList.toggle('flipped');
-    });
+    </div>`;
 
+
+
+    card.addEventListener('click', function () {
+
+        if (!timerRunning) {
+            startTimer();
+            timerRunning = true;
+        }
+
+        if (this.classList.contains('flipped') || flippedCards.length === 2) {
+            return;
+        }
+
+        this.classList.add('flipped');
+        flippedCards.push(this);
+
+        if (flippedCards.length === 2) {
+            checkIfCardsMatch()
+        }
+    });
     gameBoard.appendChild(card);
 }
+
+
 
 // timer
 
@@ -54,10 +70,36 @@ let timerRunning = false;
 const timerDisplay = document.getElementById('seconds');
 
 function startTimer() {
-    
+
     setInterval(() => {
         time++;
         timerDisplay.innerText = time;
     }, 1000);
+
 }
 
+
+function checkIfCardsMatch() {
+
+    const [card1, card2] = flippedCards;
+
+    let img1 = card1.querySelector('img').src;
+    let img2 = card1.querySelector('img').src;
+
+    if (img1 === img2) {
+        matchedCount += 2;
+        flippedCards = [];
+
+        if (matchedCount === cardCount) {
+            alert("You WON!")
+        }
+
+    } else {
+        setTimeout(() => {
+            card1.classList.remove('flipped');
+            card2.classList.remove('flipped');
+            flippedCards = [];
+
+        }, 2000);
+    }
+}
